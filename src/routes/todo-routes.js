e180733b71911e0aaa34c62ee055e415cef73eb8
Shubham <shubham@dreamkasper.com>
2026-08-12 11:12:34 +0530

    Add express-validator for todo input validation

--- src/routes/todo-routes.js
+++ src/routes/todo-routes.js
@@ -4,13 +4,14 @@ import { getTodo } from "../controllers/get-todo.js";
 import createTodo from "../controllers/create-todo.js";
 import updateTodo from "../controllers/update-todo.js";
 import deleteTodo from "../controllers/delete-todo.js";
+import { todoValidationRules, validateTodo } from "../validators/todoValidator.js";
 
 const router = express.Router();
 
 router.get("/todos", getTodos);
 router.get("/todos/:id", getTodo);
-router.post("/todos", createTodo);
-router.put("/todos/:id", updateTodo);
+router.post("/todos", todoValidationRules, validateTodo, createTodo);
+router.put("/todos/:id", todoValidationRules, validateTodo, updateTodo);
 router.delete("/todos/:id", deleteTodo);
 
 export default router;
---
+++ src/validators/todoValidator.js
@@ -0,0 +1,36 @@
+import { body, validationResult } from "express-validator";
+import { TOTO_ENUMS } from "../enums/todoEnums.js";
+
+export const todoValidationRules = [
+  body("title")
+    .trim()
+    .notEmpty()
+    .withMessage("Title is required")
+    .isLength({ max: 100 })
+    .withMessage("Title must be 100 characters or fewer"),
+  body("desc")
+    .trim()
+    .notEmpty()
+    .withMessage("Description is required")
+    .isLength({ max: 500 })
+    .withMessage("Description must be 500 characters or fewer"),
+  body("status")
+    .optional()
+    .isIn(Object.values(TOTO_ENUMS))
+    .withMessage(`Status must be one of: ${Object.values(TOTO_ENUMS).join(", ")}`),
+];
+
+export const validateTodo = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      success: false,
+      errors: errors.array().map((error) => ({
+        field: error.param,
+        message: error.msg,
+      })),
+    });
+  }
+
+  next();
+};
